7cfc787137a3e467905a099e75634abd2a828015
kduong170 <69610576+kduong170@users.noreply.github.com>
2020-09-18 00:11:33 -0700

    Add files via upload

---
+++ Healer.java
@@ -0,0 +1,40 @@
+
+public class Healer {
+
+	private String name;
+	private int level;
+	private int maxHealth;
+	private int health;
+	private int attack;
+	private int defense;
+	private int magicAttack;
+	private int magicDefense;
+	private int agility;
+	private int wisdom;
+	
+	public Healer(String n) {
+		name = n;
+		level = 1;
+		maxHealth = 16;
+		health = maxHealth;
+		attack = 4;
+		defense = 6;
+		magicAttack = 5;
+		magicDefense = 10;
+		agility = 3;
+		wisdom = 12;
+	}
+	
+	public Healer(String n, int lev) {
+		name = n;
+		level = 1;
+		maxHealth = 16*lev;
+		health = maxHealth;
+		attack = 4*lev;
+		defense = 6*lev;
+		magicAttack = 5*lev;
+		magicDefense = 10*lev;
+		agility = 3*lev;
+		wisdom = 12*lev;
+	}
+}
